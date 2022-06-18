66baff9e75d35baa8b92dd189640b54308d8c82b
David Estrich <david.estrich280@gmail.com>
2022-06-18 16:00:34 -0400

    Add personal aliases, remove atom and gitconfig

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,16 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias f='open -a Finder ./'
+
+alias indahouse="cd ~/indahouse"
+alias mrun="meteor run"
+alias ll="ls -alh"
+alias llt="ls -laht"
+
+alias reloadProfile="source ~/.zshrc clear"
+alias editProfile="code ${HOME}/.zshrc"
+alias editDot="code ${ZSH}"
+
+alias py="python3.8"
