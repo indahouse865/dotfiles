a8ab0fe0b37d4f15f0fdc5c5637bfb893dce3833
David Estrich <david.estrich280@gmail.com>
2022-06-19 14:40:07 -0400

    Fix dot script and add it as alias

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -29,3 +29,11 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 # Run the screensaver if we're in the bottom-left hot corner.
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
+
+
+# Save screenshots to the desktop
+mkdir -p ${HOME}/Screenshots
+defaults write com.apple.screencapture location -string "${HOME}/Screenshots"
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,16 +1,17 @@
-alias reload!='. ~/.zshrc'
-
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias f='open -a Finder ./'
 
 alias indahouse="cd ~/indahouse"
+alias github="cd ~/github"
 alias mrun="meteor run"
 alias ll="ls -alh"
 alias llt="ls -laht"
 
 alias reloadProfile="source ~/.zshrc clear"
-alias editProfile="code ${HOME}/.zshrc"
+alias editProfile="code ${ZSH}/zsh/zshrc.symlink"
 alias editDot="code ${ZSH}"
 
 alias py="python3.8"
+
+alias dot="${ZSH}/bin/dot"
